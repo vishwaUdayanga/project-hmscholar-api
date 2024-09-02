38baa749d39c649c1e47f16941360cc170cce4f6
JanithAmantha <151181436+JanithAmantha@users.noreply.github.com>
2024-09-03 01:53:57 +0530

    Student portal tables creation
    Initialized student portal tables

--- main.py
+++ main.py
@@ -16,6 +16,60 @@ class Admin(BaseModel):
     admin_password:str
     admin_email:str
 
+class Semester(BaseModel):
+    semester_id:str
+    year: int
+    status:str
+    semester:str
+
+class Course(BaseModel):
+    course_id:str
+    course_name: str
+    enrollment_key:str
+    course_description:str
+
+class Program(BaseModel):
+    program_id:str
+    duration: str
+    program_name:str
+
+class Affiliated_University(BaseModel):
+    affiliated_with:str
+    program_id:str
+
+class Payment(BaseModel):
+    payment_id:str
+    date:str
+    type:str
+    bank:str
+    branch:str
+    receipt_path:str
+    amount:float
+    status:str
+    student_number:str
+
+class New_student(BaseModel):
+    newStudent_id:str
+    name:str
+    address:str
+    gender:str
+    email:str
+    OL_path:str
+    AL_path:float
+    payment_id:str
+    program_id:str
+    date:str
+
+class Student(BaseModel):
+    student_id:str
+    password:str
+    newStudent_id:str
+
+class Course_semester_program(BaseModel):
+    program_id:str
+    course_id:str
+    semester_id:str
+
 def get_db():
     db = SessionLocal()
     try:
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, UUID
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, Float,  String, UUID, Date
 from database import Base
 import uuid
 
@@ -11,3 +11,67 @@ class Admin(Base):
     admin_phone=Column(String,index=True)
     admin_password=Column(String,index=True)
     admin_email=Column(String,index=True)
+
+class Semester(Base):
+    __tablename__ = 'semester'
+    semester_id=Column(String,index=True,primary_key=True)
+    year=Column(Integer,index=True)
+    status=Column(Integer,index=True)
+    semester=Column(Integer,index=True)
+
+class Course(Base):
+    __tablename__ = 'course'
+    course_id=Column(String,index=True,primary_key=True)
+    course_name=Column(String,index=True)
+    enrollment_key=Column(String,index=True)
+    course_description=Column(String,index=True)
+
+class Program(Base):
+    __tablename__ = 'program'
+    program_id=Column(UUID(as_uuid=True),index=True,primary_key=True, default=uuid.uuid4)
+    duration=Column(String,index=True)
+    program_name=Column(String,index=True)
+
+class Affiliated_University(Base):
+    __tablename__ = 'affiliated_university'
+    affiliated_with=Column(String,index=True,primary_key=True)
+    program_id=Column(UUID, ForeignKey("program.program_id"),index=True,primary_key=True)
+
+class Payment(Base):
+    __tablename__ = 'payment'
+    payment_id=Column(UUID(as_uuid=True),index=True,primary_key=True, default=uuid.uuid4)
+    date=Column(Date,index=True)
+    type=Column(String,index=True)
+    bank=Column(String,index=True)
+    branch=Column(String,index=True)
+    receipt_path=Column(String,index=True)
+    amount=Column(Float,index=True)
+    status=Column(String,index=True)
+    student_number=Column(String, ForeignKey("student.student_id"),index = True)
+
+class New_student(Base):
+    __tablename__ = 'new_student'
+    newStudent_id=Column(UUID(as_uuid=True),index=True,primary_key=True, default=uuid.uuid4)
+    name=Column(String,index=True)
+    address=Column(String,index=True)
+    gender=Column(String,index=True)
+    email=Column(String,index=True)
+    OL_path=Column(String,index=True)
+    AL_path=Column(String)
+    payment_id=Column(UUID, ForeignKey("payment.payment_id"),index=True)
+    program_id=Column(UUID, ForeignKey("program.program_id"),index=True)
+    date=Column(Date,index=True)
+
+class Student(Base):
+    __tablename__ = 'student'
+    student_id=Column(String,index=True,primary_key=True)
+    password=Column(String,index=True)
+    newStudent_id=Column(UUID, ForeignKey("new_student.newStudent_id"),index=True)
+    date=Column(Date,index=True)
+
+class Course_semester_program(Base):
+    __tablename__ = 'course_semester_program'
+    program_id=Column(UUID, ForeignKey("program.program_id"),index=True,primary_key=True)
+    course_id=Column(String, ForeignKey("course.course_id"),index=True,primary_key=True)
+    semester_id=Column(String, ForeignKey("semester.semester_id"),index=True,primary_key=True)
+
